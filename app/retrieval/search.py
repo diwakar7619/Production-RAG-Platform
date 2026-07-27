a518c015d239a9032cbda7eccc1d49711563c93d
diwakar7619 <diwakarpratham7619@gmail.com>
2026-07-27 23:08:55 +0530

    feat: Building Single Responsibility Architectecture

---
+++ app/retrieval/search.py
@@ -0,0 +1,32 @@
+from .vector_store import CLIENT, COLLECTION_NAME
+from typing import TypedDict
+
+
+class SearchResult(TypedDict):
+    text: str
+    score: float
+
+
+def search(query_embedding: list[float], limit: int = 5) -> list[SearchResult]:
+    results = CLIENT.search(
+        collection_name=COLLECTION_NAME,
+        query_vector=query_embedding,
+        limit=limit,
+    )
+
+    search_results: list[SearchResult] = []
+
+    for result in results:
+        payload = result.payload
+
+        if payload is None:
+            continue
+
+        search_result: SearchResult = {
+            "text": payload["text"],
+            "score": result.score,
+        }
+
+        search_results.append(search_result)
+
+    return search_results
--- app/retrieval/vector_store.py
+++ app/retrieval/vector_store.py
@@ -1,6 +1,8 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 
+COLLECTION_NAME = "documents"
+
 CLIENT = QdrantClient(path="./qdrant_data")
 
 if not CLIENT.collection_exists("documents"):
@@ -10,4 +12,19 @@ if not CLIENT.collection_exists("documents"):
             size=384,
             distance=models.Distance.COSINE,
         ),
+    )
+
+
+def store_embeddings(chunks: list[str], embeddings):
+    points = []
+
+    for i in range(len(chunks)):
+        point = models.PointStruct(
+            id=i, vector=embeddings[i], payload={"text": chunks[i]}
+        )
+        points.append(point)
+
+    CLIENT.upsert(
+        collection_name="documents",
+        points=points,
     )
